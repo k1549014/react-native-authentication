acf0e35edb487e9abeeadbdf5ef55bf104be504a
Tim Lee <k1549014@kingston.ac.uk>
2018-09-06 00:33:52 +0100

    stuck commit

--- server/config/datastores.js
+++
@@ -1,8 +0,0 @@
-// config/datastores.js
-
-module.exports.datastores = {
-    default: {
-        adapter: require('sails-mongo'),
-        url: 'mongodb://localhost:27017/sailsdb123',
-    }
-}
--- server/config/session.js
+++ server/config/session.js
@@ -73,7 +73,7 @@ module.exports.session = {
   * Use the latest version with Node >= 4.0                                  *
   *                                                                          *
   ***************************************************************************/
-
+   
   // adapter: 'mongo',
   // url: 'mongodb://user:password@localhost:27017/dbname', // user, password and port optional
 
